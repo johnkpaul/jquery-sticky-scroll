f18c69f2a984591865679fcefeee899ab4cb73f9
John Paul <john@johnkpaul.com>
2012-03-11 17:49:09 -0400

    update sticky scroll with class switching

--- jquery.stickyscroll.js
+++ jquery.stickyscroll.js
@@ -1,76 +1,53 @@
 (function($,window, undefined){
         
-        $.fn.scrollfix = function(){
-                
-                return $(this).each(function(index, element){
+        var pluginName = "stickyScroll",
+            defaults = {
+                "class":"scrolled-off"
+                }
 
-                        var $el = $(element), 
-                        offset = $el.offset(),
-                        position = $el.css("position");
+        function Plugin( element, options ) {
+                this.element = element;
+                this.options = $.extend( {}, defaults, options) ;
 
-                        $el.data("originalOffset",offset);
-                        $el.data("originalPosition",position);
-                        $el.data("originalMargin",$el.css("margin-top"));
-                        $.fn.scrollfix.fixedItems.push($el);
+                this._defaults = defaults;
+
+                this.init();
+        }
+        
+        Plugin.prototype.init = function(){
+                var self = this,
+                    offset = $(this.element).offset();
+                $(window).scroll(function(event){
+                       if(elementIsOutsideViewport(event.target, offset)){
+                                self.elementScrolledOff(); 
+                       } 
+                       else{
+                                self.elementScrolledOn();
+                       }
+                });
+
+        }
+
+        Plugin.prototype.elementScrolledOn = function(){
+                $(this.element).removeClass(this.options["class"]);        
+        }
+
+        Plugin.prototype.elementScrolledOff = function(){
+                $(this.element).addClass(this.options["class"]);        
+        }
+
+        $.fn.stickyScroll = function(options){
+                return this.each(function(){
+                        if (!$.data(this, 'plugin_' + pluginName)) {
+                                $.data(this, 'plugin_' + pluginName,
+                                new Plugin( this, options ));
+                        }        
                 });
         }
 
-        $.fn.scrollfix.fixedItems = [];
-
-        $(window).scroll(function(){
-            $.fn.scrollfix.fixedItems.map(function($el){
-                var offset = $el.data("originalOffset");
-                var position = $el.data("originalPosition");
-                var marginTop = $el.data("originalMargin");
-
-                if(elementIsOutsideViewport(offset)){
-                        $el.css("position","fixed").css({
-                                "top":0,
-                                "left":offset.left,
-                                "margin-top":"0px"
-                        });
-                }
-                else{
-                        $el.css("position",position).css({
-                                "top":"auto",
-                                "left":"auto",
-                                "margin-top":marginTop
-                        });
-                }
-            });
-            
-        });
-
-        function elementIsOutsideViewport(offset){
-                var $win = $(window);
-                return $win.scrollTop() > offset.top || $win.scrollLeft() > offset.left;
+        function elementIsOutsideViewport(viewport,offset){
+                var $viewport = $(viewport);
+                return $viewport.scrollTop() > offset.top || $viewport.scrollLeft() > offset.left;
         }
-})(jQuery,window);
-
-(function(window, undefined){
-        window.getComputedStylePropertyValue = function(el,cssProperty){
-                if(!window.getComputedStyle){
-                    if(document.defaultView && document.defaultView.getComputedStyle){
-                        return document.defaultView.getComputedStyle.getPropertyValue(cssProperty);
-                    }    
-                    else{
-                            var camelCasedCssProperty = getCamelCasedCssProperty(cssProperty);
-                            if(el.currentStyle){
-                                return el.currentStyle(camelCasedCssProperty);
-                            }
-                            else{
-                                return el.style[camelCasedCssProperty];
-                            }
-                    }
-                }
-                else{
-                        return window.getComputedStyle(el).getPropertyValue(cssProperty);
-                }
-                
-        }
-
-        function getCamelCasedCssProperty(cssProperty){
-                return cssProperty.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase() });
-        }
-
-})(this)
+        
+})(window.jQuery,window)
--- test/jquery.stickyscroll.tests.js
+++ test/jquery.stickyscroll.tests.js
@@ -1,77 +1,76 @@
 (function($,window, undefined){
 
-        module("scrollfix tests",{
+        module("sticky scroll tests",{
             "setup":function(){
-                this.header = $("<div class='header'></div>").prependTo(document.body)
-                .css("height","200px").css("margin-top","200px");
-                document.body.style.height="100000px";
-                this.originalOffset = this.header.offset();
+                this.$el = $("<div></div>");
+
+                this.$el.get(0).getBoundingClientRect = function(){
+                        return {left:100,top:100};
+                }
+
+                this.getStickyScrollPlugin = function($el){
+                        return $.data($el.get(0), "plugin_stickyScroll");
+                }
+
+                this.setupSpies = function(){
+                        var plugin = this.getStickyScrollPlugin(this.$el);
+                        plugin.elementScrolledOff = sinon.spy();
+                        plugin.elementScrolledOn = sinon.spy();
+                }
+            
             },
             "teardown":function(){
-                this.header.remove();
-                this.header = null;
-                $(window).scrollTop(0);
-                document.body.style.height = "auto";
-                $.fn.scrollfix.fixedItems = [];
             }    
         });
 
-        asyncTest("scrollfix set position fixed if scrolled off the page", function(){
-                this.header.scrollfix();
-                $(window).scrollTop(202);
-                var header = this.header;
-                var originalOffset = this.originalOffset;
-                setTimeout(function(){
-                        equal(header.css("top"), "0px", "top is set to 0");
-                        equal(header.css("left"), originalOffset.left+"px", "left is set to the original offset left");
-                        equal(header.offset().top - $(window).scrollTop(),0, "header is on top of the screen");
-                        equal(header.css("position"), "fixed", "header is now position fixed");
-                        start();
-                },0);
+        test("sticky scroll default class is scrolled-off", function(){
+                this.$el.stickyScroll();
+                var actualClass = this.getStickyScrollPlugin(this.$el).options["class"];
+                same(actualClass, "scrolled-off");
         });
 
-        asyncTest("scrollfix should not change css if element is not scrolled off page", function(){
-                var computedStyle = getComputedStyleCssText(this.header.get(0));
-                this.header.scrollfix();
-                $(window).scrollTop(199);
-                var header = this.header;
-                setTimeout(function(){
-                        equal(getComputedStyleCssText(header.get(0)), computedStyle, "header should not be position fixed");
-                        start();
-                },0);
+        test("sticky scroll class can be overriden", function(){
+                var newClass = "scrolled-off-fuh-real";
+                this.$el.stickyScroll({"class":newClass});
+                same(this.getStickyScrollPlugin(this.$el).options["class"], newClass);
         });
 
-        asyncTest("scrollfix should return element to original state if scroll is returned to original", function(){
-                var computedStyle = getComputedStyleCssText(this.header.get(0));
-                this.header.scrollfix();
-                $(window).scrollTop(202);
-                var header = this.header;
-                setTimeout(function(){
-                        $(window).scrollTop(0);
-                        setTimeout(function(){
-                                equal(getComputedStyleCssText(header.get(0)), computedStyle, "header should not be position fixed");
-                                start();
-                        },0);
-                },0);
+        test("if element is scrolled off viewport, correct plugin methods are called", function(){
+                this.$el.stickyScroll();
+                var plugin = this.getStickyScrollPlugin(this.$el);
+                this.setupSpies();
+                var event = jQuery.Event("scroll");
+                event.target = $({scrollTop:101});
+                $(window).trigger(event);
+                equals(plugin.elementScrolledOff.called, true);
+                equals(plugin.elementScrolledOn.called, false);
         });
 
-})(jQuery,window);
+        test("if element is scrolled into viewport, correct plugin methods are called", function(){
+                this.$el.stickyScroll();
+                var plugin = this.getStickyScrollPlugin(this.$el);
+                this.setupSpies();
+                var event = jQuery.Event("scroll");
+                event.target = $({scrollTop:99});
+                $(window).trigger(event);
+                equals(plugin.elementScrolledOff.called, false);
+                equals(plugin.elementScrolledOn.called, true);
+        });
+
+        test("elementScrolledOff function adds class", function(){
+                this.$el.stickyScroll();
+                var plugin = this.getStickyScrollPlugin(this.$el);
+                plugin.elementScrolledOff();
+                equals(this.$el.hasClass("scrolled-off"), true);
+        });
+
+        test("elementScrolledOn function remove class", function(){
+                this.$el.stickyScroll();
+                var plugin = this.getStickyScrollPlugin(this.$el);
+                plugin.elementScrolledOn();
+                equals(this.$el.hasClass("scrolled-off"), false);
+        });
+
+})(window.jQuery,window);
 
 
-function getComputedStyleCssText(element){
-        var cssObject = window.getComputedStyle(element),
-            prop,
-            cssText, 
-            cssAccumulator = [];
-
-        if(cssObject.cssText != ""){
-            return cssObject.cssText;
-        }
-
-        for(prop in cssObject){
-            if(typeof cssObject[prop] == "string"){
-                cssAccumulator.push(prop + " : " + cssObject[prop]);        
-            }
-        }
-        return cssAccumulator.join("; ");
-}
